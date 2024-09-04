06ff7f564c8187ec66a8b6e1c8c10ea02eed929f
singhalatul <atul.aggarwal2852@gmail.com>
2024-09-04 10:13:20 +0530

    design higher order function for not repeting code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (func)=>{
+    return (req,res,next)=>{
+        Promise.resolve(func(req,res,next))
+        .catch((err)=>{
+            next(err);
+        })
+    }
+}
+
+
+// const asyncHandler = (func)=>{
+//     async(req,res,next)=>{
+//         try {
+//             func(req,res,next)
+//         } catch (error) {
+//             res.send(err.code||500).json({
+//                 success:false,
+//                 message:err.message
+//             })
+//         }
+//     }
+// }
+
