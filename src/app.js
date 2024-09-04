d035b38f2acb49578868f58f25dc90089c3e5911
singhalatul <atul.aggarwal2852@gmail.com>
2024-09-04 09:40:41 +0530

    Error handling with nodejs Error class

--- src/app.js
+++ src/app.js
@@ -8,11 +8,16 @@ app.use(cors({
     credentials:true
 }))
 
+//setup for security purposes
 app.use(express.json({limit:"16kb"}));
 
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 
 app.use(express.static("public"));
+
+//setup routes
+
+
 
 
 export default app;
